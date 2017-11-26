f9877fd218e5b198ff13b09a0ef95da10c003a46
cwh <cweihang@foxmail.com>
2017-11-26 21:08:16 +0800

    initial commit

---
+++ src/main/java/scut/cwh/reid/ReidApplication.java
@@ -0,0 +1,12 @@
+package scut.cwh.reid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReidApplication.class, args);
+	}
+}
---
+++ src/test/java/scut/cwh/reid/ReidApplicationTests.java
@@ -0,0 +1,16 @@
+package scut.cwh.reid;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReidApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
